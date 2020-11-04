fe24e9760628e87c8e2e0ff6a9e95abd7da53106
Richard Lehmann <lehmaric@b-tu.de>
2020-11-04 11:33:56 +0100

    Ordnerstruktur geaendert

---
+++ vorgabe1/src/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "gui/DrawingWindow.h"
+
+using namespace std;
+using namespace GUI;
+
+int main(int argc, char** argv)
+{
+    // gui example code
+    DrawingWindow window(800,600,"gui");
+
+    window.setBackgroundColor(RGBColor(0,0,0));
+
+    window.setForegroundColor(RGBColor(0,0,255));
+    window.drawFilledRectangle(100,100,100,100);
+
+    window.setForegroundColor(RGBColor(0,255,0));
+    window.drawRectangle(250,250,100,150);
+    
+    window.drawText(10,200,"test");
+
+	// run until user presses q on console
+	char input = ' ';
+	while(input != 'q')
+	{
+		cout << "Press q and enter to quit" << endl;
+		cin >> input;
+		cout << "read: '" << input << "' from console" << endl;
+	}
+
+	return 0;
+}
+
